bc3ec5555a95355dfc6832dcd5e38ada98ff6e45
Clint Rutkas <crutkas@microsoft.com>
2026-08-21 12:25:26 -0700

    arm64 scanner: include dos2unix x64 removals
    Signed-off-by: Clint Rutkas <crutkas@microsoft.com>

--- t/check-arm64-busybox-architecture.sh
+++ t/check-arm64-busybox-architecture.sh
@@ -317,9 +317,9 @@ die "The authoritative release architecture counts changed"
 test -59 = "$((current_busybox_x64 - current_leaf_x64))" &&
 test 61 = "$((current_busybox_arm64 - current_leaf_arm64))" ||
 die "The current file-list BusyBox architecture delta is not -59 x64/+61 ARM64"
-test -14 = "$((current_combined_x64 - current_busybox_x64))" &&
+test -20 = "$((current_combined_x64 - current_busybox_x64))" &&
 test 20 = "$((current_combined_arm64 - current_busybox_arm64))" ||
-die "The current file-list OpenSSH architecture delta is not -14 x64/+20 ARM64"
+die "The current file-list OpenSSH architecture delta is not -20 x64/+20 ARM64"
 test "$current_leaf_x86" = "$current_busybox_x86" &&
 test "$current_leaf_x86" = "$current_combined_x86" &&
 test "$current_leaf_anycpu" = "$current_busybox_anycpu" &&
