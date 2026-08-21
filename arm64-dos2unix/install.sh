d849a7334e4251ec14572b00ea18b013e26374ec
Clint Rutkas <crutkas@microsoft.com>
2026-08-21 10:33:00 -0700

    arm64 dos2unix: remove source aliases after copying all outputs
    Signed-off-by: Clint Rutkas <crutkas@microsoft.com>

--- arm64-dos2unix/install.sh
+++ arm64-dos2unix/install.sh
@@ -98,6 +98,10 @@ do
 	src=${tool_src##*:}
 	"$pwsh_exe" -NoLogo -NoProfile -Command "Copy-Item -LiteralPath '$src_dir_win\\$src.exe' -Destination '$dst_dir_win\\$tool.exe' -Force" ||
 	die "Could not materialize /usr/bin/$tool.exe from $package_path"
-	"$pwsh_exe" -NoLogo -NoProfile -Command "Remove-Item -LiteralPath '$src_dir_win\\$src.exe' -Force" ||
+done
+
+for tool in d2u dos2unix unix2dos unix2mac
+do
+	"$pwsh_exe" -NoLogo -NoProfile -Command "Remove-Item -LiteralPath '$src_dir_win\\$tool.exe' -Force" ||
 	die "Could not remove /clangarm64/bin/$tool.exe"
 done
